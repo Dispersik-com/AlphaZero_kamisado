b109522adb1fe5be6a9158903cb0445bb381b1e3
Vladimir_k <126075849+Dispersik-com@users.noreply.github.com>
2024-01-20 15:50:58 +0200

    Update kamisado_environment.py & add numpy

--- kamisado_environment.py
+++ kamisado_environment.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Monk:
     def __init__(self, command_color, self_color):
         self.command_color = command_color
@@ -10,7 +12,7 @@ class Monk:
 class KamisadoEnvironment:
 
     # Initializing the colored board
-    color_board = [
+    color_board = np.array([
         [7, 6, 5, 4, 3, 2, 1, 0],  # O A V P Y R G B
         [2, 7, 4, 1, 6, 3, 0, 5],  # R O P G A Y B V
         [1, 4, 7, 2, 5, 0, 3, 6],  # G P O R V B Y A
@@ -19,7 +21,7 @@ class KamisadoEnvironment:
         [6, 3, 0, 5, 2, 7, 4, 1],  # A Y B V R O P G
         [5, 0, 3, 6, 1, 4, 7, 2],  # V B Y A G P O R
         [0, 1, 2, 3, 4, 5, 6, 7]   # B G R Y P V A O
-    ]
+    ])
 
     # Dictionary for visual representation of colors
     color_dict = {
@@ -34,17 +36,20 @@ class KamisadoEnvironment:
     }
 
     def __init__(self):
-        self.game_board = [[None for _ in range(8)] for _ in range(8)]
+        # Initializing the game board with None (empty spaces)
+        self.game_board = np.full((8, 8), None)
         self.current_player = "White"
-
+        self.last_move = None
+        # Initializing the monks on the board
         self.init_monk()
 
     def init_monk(self):
         # Initializing the initial distribution of monks
-        self.game_board[0] = [Monk("Black", self.color_dict[i]) for i in range(8)]
+        self.game_board[0] = [Monk("Black", self.color_dict[7-i]) for i in range(8)]
         self.game_board[7] = [Monk("White", self.color_dict[i]) for i in range(8)]
 
     def print_board(self):
+        # Printing the game board
         for i in range(8):
             for j in range(8):
                 cell_color = self.color_dict[self.color_board[i][j]]
@@ -55,57 +60,129 @@ class KamisadoEnvironment:
                     print(f"E-{cell_color} ", end="")
             print()
 
-    def make_move(self, start_row, start_col, end_row, end_col):
+    def make_move(self, start_cell, end_cell):
+
+        start_row, start_col, end_row, end_col = start_cell, end_cell
+
+        # Checking if the move is valid
         if not self.is_valid_move(start_row, start_col, end_row, end_col):
             print("Invalid move!")
             return
 
+        # Moving the monk to the new position
         self.game_board[end_row][end_col] = self.game_board[start_row][start_col]
         self.game_board[start_row][start_col] = None
 
-        # Checking for opponent block
-        if 0 < end_row < 7 and self.game_board[end_row + 1][end_col]:
-            self.game_board[end_row + 1][end_col] = None
+        # init tuple of last move
+        self.last_move = ((end_row, end_col), self.color_board[end_row][end_col])
 
+        # Switching the current player for the next turn
         self.switch_player()
 
+    def _get_diagonals_by_cell(self, row, col):
+
+        matrix = self.game_board
+
+        diagonal_indices, diagonal_objects = None, None
+        offset_by_object = matrix.shape[1] - 1 - col - row
+
+        if self.current_player == "Black":
+
+          main_diagonal = matrix.diagonal(col - row)[col+1:]
+          secound_diagonal = matrix[:, ::-1].diagonal(offset_by_object)[offset_by_object+1:]
+
+          diagonal_objects = main_diagonal, secound_diagonal
+
+          main_indices = [(row+i+1, col+i+1)for i in range(len(main_diagonal))]
+          secound_indices = [(row+i, col-i-1)for i in range(len(secound_diagonal))]
+
+          diagonal_indices = main_indices, secound_indices
+
+        else:
+
+          main_diagonal = matrix.diagonal(col - row)[:col]
+          secound_diagonal = matrix[:, ::-1].diagonal(offset_by_object)[:offset_by_object]
+
+          diagonal_objects = main_diagonal, secound_diagonal
+
+          main_indices = [(row-i-1, col-i-1)for i in range(len(main_diagonal))]
+          secound_indices = [(row-i, col+i+1)for i in range(len(secound_diagonal))]
+
+          diagonal_indices = main_indices, secound_indices
+
+        return diagonal_indices, diagonal_objects
+
+    def _get_sright_line_by_cell(self, row, col):
+        
+        if self.current_player == "Black":
+          stright_line_objects = self.game_board[:,col].T[row:]
+          steps = len(stright_line_objects)
+          stright_line_indices = list(enumerate([col]*steps))[::-1][row+1:]
+        else:
+          stright_line_objects = self.game_board[:,col].T[:row+1]
+          stright_line_indices = list(enumerate([col]*row))[::-1][:row+1]
+
+        return stright_line_indices, stright_line_objects
+
+    def get_legal_moves(self):
+        if self.last_move is not None:
+          cell, _ = self.last_move
+          giagonals = self._get_diagonals_by_cell(cell)
+          stright_line = self._get_stright_by_cell(cell)
+          return giagonals, stright_line
+        else:
+          indices = np.argwhere(self.game_board[1:])
+          coordinates = [(i[0] + 1, i[1]) for i in indices]
+          return coordinates
+
     def is_valid_move(self, start_row, start_col, end_row, end_col):
-      if not (0 <= start_row < 8 and 0 <= start_col < 8 and 0 <= end_row < 8 and 0 <= end_col < 8):
-          return False
+        # Check if the move is within the boundaries of the board
+        if not (0 <= start_row < 8 and 0 <= start_col < 8 and 0 <= end_row < 8 and 0 <= end_col < 8):
+            return False
 
-      monk = self.game_board[start_row][start_col]
-      if not monk or monk.command_color != self.current_player:
-          return False
+        # Get the monk at the starting position
+        monk = self.game_board[start_row][start_col]
 
-      if not (0 <= end_row < 8 and 0 <= end_col < 8 and self.game_board[end_row][end_col] is None):
-          return False
+        # Check if there is a monk at the starting position and if it belongs to the current player
+        if monk is None or monk.command_color != self.current_player:
+            return False
 
-      # Проверка на правильное направление движения
-      if self.current_player == "White" and end_row >= start_row:
-          return False
-      elif self.current_player == "Black" and end_row <= start_row:
-          return False
+        # Check if the destination position is empty
+        if not (0 <= end_row < 8 and 0 <= end_col < 8 and self.game_board[end_row][end_col] is None):
+            return False
 
-      # Проверка на перемещение по клеткам того же цвета
-      if not self.is_valid_color_move(start_row, start_col, end_row, end_col):
-          return False
+        # Check for the correct direction of movement based on the current player
+        if self.current_player == "White" and end_row >= start_row:
+            return False
+        elif self.current_player == "Black" and end_row <= start_row:
+            return False
 
-      return True
+        # Check for movement on cells of the same color
+        if not self.is_valid_color_move(start_row, start_col, end_row, end_col):
+            return False
+
+        return True
 
     def is_valid_color_move(self, start_row, start_col, end_row, end_col):
-        start_color = (start_row + start_col) % 2
-        end_color = (end_row + end_col) % 2
-        return start_color == end_color
+        if self.last_move is None:
+          return True
+
+        monk_color = self.game_board[start_row][start_row].self_color
+        last_cell_color = self.color_dict[self.last_move[1]]
+        # Check if the monk is moving on cells of the same color
+        return monk_color == last_cell_color
 
     def switch_player(self):
+        # Switching the current player for the next turn
         self.current_player = "Black" if self.current_player == "White" else "White"
 
     def check_winner(self):
+        # Check if any player has reached the opposite end and declare the winner
         for i in range(8):
-            if self.game_board[0][i] and self.game_board[0][i].command_color == "Black":
+            if self.game_board[7][i] and self.game_board[7][i].command_color == "Black":
                 print("Black player wins!")
                 return "Black"
-            if self.game_board[7][i] and self.game_board[7][i].command_color == "White":
+            if self.game_board[0][i] and self.game_board[0][i].command_color == "White":
                 print("White player wins!")
                 return "White"
         return None
