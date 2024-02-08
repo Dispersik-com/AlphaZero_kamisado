a07c8328fc034779362edb7094a48e70879d0649
Dispersik-com <harvel.bk@gmail.com>
2024-02-08 17:23:39 +0200

    add NeuralMonteCarloTreeSearch class

--- MCTS.py
+++ MCTS.py
@@ -306,3 +306,35 @@ class MonteCarloTreeSearch:
         with open(filename, 'r') as file:
             tree_data = json.load(file)
         return tree_data
+
+
+class NeuralMonteCarloTreeSearch(MonteCarloTreeSearch):
+    def __init__(self, game, policy_network, value_network, exploration_weight=1.0):
+        super().__init__(game, exploration_weight)
+        # add policy and value networks
+        self.policy_network = policy_network
+        self.value_network = value_network
+
+    def simulate(self, node):
+        """
+        Simulate a game starting from the given state.
+
+        Args:
+            node: The initial node of the game.
+
+        Returns:
+            The reward of the simulation.
+        """
+        pass
+
+    def convert_node_to_input(self, node):
+        """
+        Convert the node to a format suitable for input to the neural network.
+
+        Args:
+            node: The MCTS node.
+
+        Returns:
+            Input data for the neural network.
+        """
+        pass
