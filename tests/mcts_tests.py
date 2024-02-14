83a117910094f7e57ec91200b8dc179da423cebb
Dispersik-com <harvel.bk@gmail.com>
2024-02-14 17:44:32 +0200

    update mcts_tests.py

--- tests/mcts_tests.py
+++ tests/mcts_tests.py
@@ -2,6 +2,7 @@ import unittest
 
 from MCTS.mcts import MonteCarloTreeSearch
 from kamisado_environment.kamisado_enviroment import KamisadoGame
+from MCTS.node import MCTSNode
 
 
 class TestMonteCarloTreeSearch(unittest.TestCase):
@@ -10,6 +11,17 @@ class TestMonteCarloTreeSearch(unittest.TestCase):
         self.game = KamisadoGame()
         self.mcts = MonteCarloTreeSearch(self.game)
 
+    def test_simulate(self):
+        node = self.mcts.root
+        last_node, reward = self.mcts.simulate(node)
+        self.assertIsInstance(last_node, MCTSNode)
+        self.assertIsNotNone(reward)
+
+    def test_select(self):
+        node = self.mcts.root
+        child_node = self.mcts.select(node)
+        self.assertIsNotNone(child_node)
+
     def test_search_integration(self):
         self.mcts = MonteCarloTreeSearch(self.game)
-        self.mcts.search(num_simulations=100)
+        self.mcts.search(num_simulations=1000)
